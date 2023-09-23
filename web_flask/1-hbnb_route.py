636b1ecaafb9629c2eadb1fa2559cd52339d18b9
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-09-23 23:29:13 +0000

    fixed error in 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# starts a Flask web application
-
+''' starts a Flask web application
+'''
 from flask import Flask
 
 
@@ -20,4 +20,4 @@ def hbnb_route():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run(host='0.0.0.0', port=5000)
